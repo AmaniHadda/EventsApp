ff333bd5af1fd12f1fd89af3fa7631c2c63dfe2b
unknown <amani.hadda@esprit.tn>
2023-11-19 00:17:18 +0100

    EventApp

---
+++ Event/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+from .views import *
+
+urlpatterns = [
+    path('i/',views.index),
+    path('index/<str:param>',views.index_param),
+    path('Affiche/',Affiche,name='Aff'),
+    path('Liste/',AfficheGeneric.as_view()),
+    path('Detail/<str:title>',Detail,name="D"),
+    path('DG/<int:pk>',DetailGeneric.as_view(),name="DD"),
+    path('Ajout/',Add,name="Add"),
+    path('Add/',Ajout.as_view(),name='Ajout'),
+    path('Delete/<int:pk>',Delete.as_view(),name="delete"),
+    path('Deletee/<int:id>',Deletee,name="deletee"),
+    path('Part/<int:event_id>',Participate,name="Participate"),
+    path('Cancel/<int:id>',Cancel,name="Cancel"),
+    path('update/<int:pk>',UpdateGeneric.as_view(),name='update'),
+    # path('api/',get_Events),
+    path('search/',Search,name='search'),
+    path('up/<str:title>',Update,name='up'),
+
+
+]
